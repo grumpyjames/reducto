376b3044732b1711c8114b806b667ce636796915
James Byatt <grumpyjames@gmail.com>
2017-11-21 22:53:18 +0000

    A couple of Bucketing improvements

--- src/main/java/net/digihippo/timecache/Bucketing.java
+++ src/main/java/net/digihippo/timecache/Bucketing.java
@@ -24,11 +24,11 @@ final class Bucketing
         final long fromMillis = fromInclusive.toInstant().toEpochMilli();
         final long toMillis = toExclusive.toInstant().toEpochMilli();
         final long firstBucketKey = (fromMillis / bucketSizeMillis) * bucketSizeMillis;
-        final long remainder = (toMillis - firstBucketKey) % bucketSizeMillis;
         long requiredBucketCount = (toMillis - firstBucketKey) / bucketSizeMillis;
+        final long remainder = (toMillis - firstBucketKey) % bucketSizeMillis;
         if (remainder != 0)
         {
-            requiredBucketCount = 1 + ((toMillis - firstBucketKey) / bucketSizeMillis);
+            requiredBucketCount += 1;
         }
 
         return new Buckets(firstBucketKey, requiredBucketCount);
@@ -37,14 +37,7 @@ final class Bucketing
     static long upToMultiple(final long bucketSize, final long epochMilli)
     {
         long remainder = epochMilli % bucketSize;
-        if (remainder == 0)
-        {
-            return epochMilli;
-        }
-        else
-        {
-            return epochMilli - remainder + bucketSize;
-        }
+        return remainder == 0 ? epochMilli : epochMilli - remainder + bucketSize;
     }
 
     private Bucketing() {}
